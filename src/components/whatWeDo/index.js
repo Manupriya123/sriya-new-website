753debd73b388d5d371be6660f449dcc8dec2479
Manupriya <manupriya.gupta98@gmail.com>
2023-05-22 11:52:39 +0530

    master branch

--- src/components/whatWeDo/index.js
+++ src/components/whatWeDo/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const WhatWeDo = () => {
+const WhatWeDo = ({props}) => {
+  console.log("props : ",props)
   return (
-    <div>
-      
+    <div className='container_wrapper'>
+      <h2>{props.heading}</h2>
+      <p>{props.content}</p>
     </div>
   )
 }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Header from "../../components/header";
 import HomeBanner from "../../components/homeBanner";
+import WhatWeDo from "../../components/whatWeDo";
 
 const Home = () => {
+  let whatData = {
+    heading: "WHAT WE DO",
+    content:
+      "We’re a deep tech AI-ML leader with a proprietary DATA-PLT (Predictive Learning Transformer) suite of technologies and SXI.  We provide business leaders a new way to SCORE, CORRELATE and IMPROVE their companies.",
+  };
   return (
     <div>
       <Header />
       <HomeBanner />
+      <WhatWeDo props={whatData} />
     </div>
   );
 };
